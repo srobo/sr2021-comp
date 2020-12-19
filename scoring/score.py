52aee538e1c7bc12f7b4e5c8789e5c21d9a4fe7e
Peter Law <PeterJCLaw@gmail.com>
2020-12-19 23:36:12 +0000

    We have nothing of interest to validate this year
    In theory we could validate the station codes, but since we know
    that they're being recorded fully automatically it doesn't seem
    worth it.

--- scoring/score.py
+++ scoring/score.py
@@ -1,15 +1,8 @@
-class InvalidScoresheetException(Exception):
-    pass
-
-
 class Scorer(object):
     def __init__(self, teams_data, arena_data):
         self._teams_data = teams_data
 
     def calculate_scores(self):
-        raise NotImplementedError
-
-    def validate(self, extra_data):
         raise NotImplementedError
 
 
--- scoring/tests/test_scoring.py
+++ scoring/tests/test_scoring.py
@@ -10,7 +10,7 @@ import sys
 ROOT = pathlib.Path(__file__).parent.parent
 sys.path.insert(0, str(ROOT))
 
-from score import Scorer, InvalidScoresheetException
+from score import Scorer
 
 
 class ScorerTests(unittest.TestCase):
@@ -42,8 +42,6 @@ class ScorerTests(unittest.TestCase):
 
         scorer = Scorer(teams_data, arena_data)
         scores = scorer.calculate_scores()
-
-        scorer.validate(extra_data)
 
         self.assertEqual(
             teams_data.keys(),
