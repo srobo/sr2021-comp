3eeab4e9793487b7744d2f4d9b07bd3ffbf2bf48
Peter Law <PeterJCLaw@gmail.com>
2020-12-19 23:38:28 +0000

    Record an assumption

--- scoring/score.py
+++ scoring/score.py
@@ -1,4 +1,8 @@
 class Scorer(object):
+    # Assumption: we will trust the *ordering* of the entries in the data we
+    # are given and *ignore* the actual 'time' values. We defer handling of
+    # equialent-time operations to the territory controller.
+
     def __init__(self, teams_data, arena_data):
         self._teams_data = teams_data
 
