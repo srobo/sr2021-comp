8c0b28e92ec926479e10e584ab13d90943d15acf
Will Barber <WillB97@users.noreply.github.com>
2021-04-22 16:37:40 +0100

    Update score.py to support different zones being worth different points
    The updated logic is copied from srobo/competition-simulator where it has been tested

--- scoring/score.py
+++ scoring/score.py
@@ -1,6 +1,13 @@
 import collections
 
-POINTS_PER_TERRITORY = 2
+DEFAULT_POINTS_PER_TERRITORY = 2
+EXTRA_VALUE_TERRITORIES = {
+    'TH': 4,
+    'FL': 4,
+    'SF': 4,
+    'HA': 4,
+    'YT': 8,
+}
 
 
 class MaximumExtentScorer(object):
@@ -71,7 +78,7 @@ class EndStateScorer(object):
 
         return {
             tla: sum(
-                POINTS_PER_TERRITORY
+                EXTRA_VALUE_TERRITORIES.get(territory, DEFAULT_POINTS_PER_TERRITORY)
                 for territory in zone_to_territories.get(zone, [])
             )
             for zone, tla in self._zone_to_tla.items()
